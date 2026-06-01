9e62714688dbae0da7afde628995db83274eb33d
caldera <faainamacaldera16@gmail.com>
2026-06-01 16:51:30 +0700

    style: enhancement for logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,39 +1,50 @@
 import pino, { type LoggerOptions } from 'pino'
 import env from '@/configs/env'
 
-const isDev = env.NODE_ENV !== 'production'
+const { NODE_ENV, LOG_LEVEL } = env
+const isDev = NODE_ENV !== 'production'
 
-const devTransport: LoggerOptions['transport'] = {
-    target: 'pino-pretty',
-    options: {
-        colorize: true,
-        colorizeObjects: true,
-        translateTime: 'SYS:HH:MM:ss',
-        ignore: 'pid,hostname',
-        levelFirst: true,
-        customColors:
-            'trace:gray,debug:cyanBright,info:greenBright,warn:yellowBright,error:redBright,fatal:magentaBright',
-        messageFormat: '  ▸  {msg}',
-        errorLikeObjectKeys: ['err', 'error'],
-        errorProps: 'stack,type,statusCode,isOperational',
-        singleLine: false
+const devOptions: LoggerOptions = {
+    level: LOG_LEVEL,
+    transport: {
+        target: 'pino-pretty',
+        options: {
+            colorize: true,
+            colorizeObjects: true,
+            singleLine: false,
+            levelFirst: true,
+            translateTime: 'SYS:HH:MM:ss',
+            ignore: 'pid,hostname',
+            messageFormat: '  ▸  {msg}',
+            errorLikeObjectKeys: ['err', 'error'],
+            errorProps: 'stack,type,statusCode,isOperational',
+            customColors: [
+                'trace:gray',
+                'debug:cyanBright',
+                'info:greenBright',
+                'warn:yellowBright',
+                'error:redBright',
+                'fatal:magentaBright'
+            ].join(',')
+        }
     }
 }
 
 const prodOptions: LoggerOptions = {
-    level: env.LOG_LEVEL,
+    level: LOG_LEVEL,
     base: null,
+    timestamp: pino.stdTimeFunctions.isoTime,
     formatters: {
         level(label) {
             return { level: label.toUpperCase() }
         }
     },
-    timestamp: pino.stdTimeFunctions.isoTime,
     serializers: {
         err: pino.stdSerializers.err,
         req: pino.stdSerializers.req,
         res: pino.stdSerializers.res
     },
+
     redact: {
         paths: [
             '*.password',
@@ -50,6 +61,4 @@ const prodOptions: LoggerOptions = {
     }
 }
 
-export const logger = isDev
-    ? pino({ level: env.LOG_LEVEL, transport: devTransport })
-    : pino(prodOptions)
+export const logger = pino(isDev ? devOptions : prodOptions)
