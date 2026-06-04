aa88299c62a54af3ae750b90ec0eff9361ccd141
caldera <faainamacaldera16@gmail.com>
2026-06-04 19:21:52 +0700

    feat: add cadre rroute

--- src/routes/index-routes.ts
+++ src/routes/index-routes.ts
@@ -3,9 +3,12 @@ import { Router } from 'express'
 import healthRoutes from '@/routes/health-routes'
 import userRoutes from '@/routes/user-routes'
 
+import cadreRoutes from '@/routes/cadre-route'
+
 const router = Router()
 
 router.use('/api/health', healthRoutes)
 router.use('/api/users', userRoutes)
+router.use('/api/cadres', cadreRoutes)
 
 export default router
