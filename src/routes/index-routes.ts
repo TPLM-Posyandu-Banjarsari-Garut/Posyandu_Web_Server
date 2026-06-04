45bd10564fd261e9d1c56dd23364e3991960c4ff
caldera <faainamacaldera16@gmail.com>
2026-06-04 20:25:05 +0700

    feat: add midwife route

--- src/routes/index-routes.ts
+++ src/routes/index-routes.ts
@@ -4,7 +4,6 @@ import healthRoutes from '@/routes/health-routes'
 import userRoutes from '@/routes/user-routes'
 
 import cadreRoutes from '@/routes/cadre-route'
-import midwifeRoutes from '@/routes/midwife-route'
 import parentRoutes from '@/routes/parent-route'
 
 const router = Router()
@@ -12,7 +11,7 @@ const router = Router()
 router.use('/api/health', healthRoutes)
 router.use('/api/users', userRoutes)
 router.use('/api/cadres', cadreRoutes)
-router.use('/api/midwifes', midwifeRoutes)
+router.use('/api/midwifes')
 router.use('/api/parents', parentRoutes)
 
 export default router
