34d4d2aed498784a2f91ca5773d12a9f90467f62
caldera <faainamacaldera16@gmail.com>
2026-06-09 11:01:14 +0700

    fix: using cdn packages to swagger ui

--- src/configs/swagger.ts
+++ src/configs/swagger.ts
@@ -1,12 +1,22 @@
 import swaggerUi from 'swagger-ui-express'
 import { Express } from 'express'
-import env from '@/configs/env'
 import { generateOpenApiDocs } from '@/configs/swagger-generator'
 
 export const setupSwagger = (app: Express): void => {
-    if (env.NODE_ENV !== 'development') return
-
     const swaggerDocument = generateOpenApiDocs()
 
-    app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+    // Menggunakan CDN assets untuk Swagger UI agar tidak terkendala MIME type di Vercel/Serverless
+    const options = {
+        customCssUrl: 'https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui.css',
+        customJs: [
+            'https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui-bundle.js',
+            'https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui-standalone-preset.js'
+        ]
+    }
+
+    app.use(
+        '/api/docs',
+        swaggerUi.serve,
+        swaggerUi.setup(swaggerDocument, options)
+    )
 }
